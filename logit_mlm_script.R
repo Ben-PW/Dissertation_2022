56aa3aebc10e49acc1f45921fc37a883aad813e7
yorkshireorange <99730135+yorkshireorange@users.noreply.github.com>
2022-07-13 05:33:10 +0200

    Code for logit MLM with sample analyses

---
+++ logit_mlm_script.R
@@ -0,0 +1,89 @@
+### SETUP
+
+require(here)
+require(lme4) #glmer for MLMs
+require(lmerTest) # sig. values for glmer
+
+# read data
+redcard <- read.csv(here('Data', 'CrowdstormingDataJuly1st.csv'), stringsAsFactors = FALSE)
+### Logit MLM
+
+# 1. DV (redCards) needs to be restructured as dichotomous:
+summary(as.factor(redcard$redCards))
+
+# replacing the value of 2 with 1
+redcard["redCards"][redcard["redCards"] == 2] <- 1
+
+# null GLM logit model
+mod0 <- glm(redCards ~ 1,
+            family = binomial(link="logit"),
+            data = redcard)
+summary(mod0)
+
+# random intercept for players
+mod1 <- glmer(redCards ~ 1 + (1 | playerShort),
+              data = redcard,
+              family = binomial(link="logit"),
+              control = glmerControl(optimizer = "bobyqa"),
+              nAGQ = 0)
+summary(mod1)
+
+# random intercept for players & referees
+mod1.1 <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum),
+              data = redcard,
+              family = binomial(link="logit"),
+              control = glmerControl(optimizer = "bobyqa"),
+              nAGQ = 0)
+summary(mod1.1)
+
+# compare models
+anova(mod1, mod1.1)
+
+# random intercept for players, referees, & league country
+mod1.x <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum)
+                + (1 | leagueCountry),
+                data = redcard,
+                family = binomial(link="logit"),
+                control = glmerControl(optimizer = "bobyqa"),
+                nAGQ = 0)
+summary(mod1.x)
+# no additional variance explained by league country
+
+# random intercept for players, referees, & league country
+# fixed effects of player position
+mod2 <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum)
+                + position,
+                data = redcard,
+                family = binomial(link="logit"),
+                control = glmerControl(optimizer = "bobyqa"),
+                nAGQ = 0)
+summary(mod2)
+
+# compare models
+anova(mod1, mod1.1, mod2)
+
+mod2x <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum)
+              + as.factor(position),
+              data = redcard,
+              family = binomial(link="logit"),
+              control = glmerControl(optimizer = "bobyqa"),
+              nAGQ = 0)
+summary(mod2x)
+
+#as.factor approach gives different estimates - why?
+
+anova(mod1, mod1.1, mod2, mod2x)
+
+# random intercept for players & referees
+# fixed effects of player position
+# random effect of player over referee
+mod3 <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum)
+               + (playerShort | refNum) + as.factor(position),
+               data = redcard,
+               family = binomial(link="logit"),
+               control = glmerControl(optimizer = "bobyqa"),
+               nAGQ = 0)
+summary(mod3)
+# I do not have the computational power to do this:
+# cannot allocate vector of size 10.9 Gb
+anova(mod1, mod1.1, mod2, mod2x, mod3)
