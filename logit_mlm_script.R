60ff405c9892376d649288a47f7325e1b85d90cd
yorkshireorange <99730135+yorkshireorange@users.noreply.github.com>
2022-07-15 15:06:57 +0200

    Added R2

--- logit_mlm_script.R
+++ logit_mlm_script.R
@@ -2,7 +2,10 @@
 
 require(here)
 require(lme4) #glmer for MLMs
+# read https://cran.r-project.org/web/packages/lme4/lme4.pdf for interpretation.
 require(lmerTest) # sig. values for glmer
+require(MuMIn)
+# read https://cran.r-project.org/web/packages/MuMIn/MuMIn.pdf for interpretation
 
 # read data
 redcard <- read.csv(here('Data', 'CrowdstormingDataJuly1st.csv'), stringsAsFactors = FALSE)
@@ -38,7 +41,7 @@ mod1 <- glmer(redCards ~ 1 + (1 | playerShort),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 summary(mod1)
-
+r.squaredGLMM(mod1)
 # random intercept for players & referees
 mod1.1 <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum),
               data = redcard,
@@ -46,7 +49,7 @@ mod1.1 <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 summary(mod1.1)
-
+r.squaredGLMM(mod1.1)
 # compare models
 anova(mod1, mod1.1)
 
@@ -69,7 +72,7 @@ mod2 <- glmer(redCards ~ position + avrate + (1 | playerShort)
                 control = glmerControl(optimizer = "bobyqa"),
                 nAGQ = 0)
 summary(mod2)
-
+r.squaredGLMM(mod2)
 # compare models
 anova(mod1, mod1.1, mod2)
 
@@ -80,7 +83,7 @@ mod2x <- glmer(redCards ~ as.factor(position) + avrate
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 summary(mod2x)
-
+r.squaredGLMM(mod2x)
 #as.factor approach gives different estimates - why?
 
 anova(mod1, mod1.1, mod2, mod2x)
@@ -98,3 +101,4 @@ mod3 <- glmer(redCards ~ position + avrate +  (1 | playerShort)
 # "cannot allocate vector of size 10.9 Gb"
 #sink()
 summary(mod3)
+r.squaredGLMM(mod3)
