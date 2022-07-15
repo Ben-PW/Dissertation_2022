a88ad25812622231605e1cd92b051f8d1fd5c7d2
yorkshireorange <99730135+yorkshireorange@users.noreply.github.com>
2022-07-15 19:12:33 +0200

    Included variance extraction, revised models.

--- logit_mlm_script.R
+++ logit_mlm_script.R
@@ -4,12 +4,14 @@ require(here)
 require(lme4) #glmer for MLMs
 # read https://cran.r-project.org/web/packages/lme4/lme4.pdf for interpretation.
 require(lmerTest) # sig. values for glmer
-require(MuMIn) #R Squared
-# read https://cran.r-project.org/web/packages/MuMIn/MuMIn.pdf for interpretation
-require(oddsratio)
-# generates odds ratios for GLMM output; not sure how this works?
+require(insight)
+# extract R-squared for fixed, random, and mixed effects
 require(sjPlot)
 # creates nice overview tables.
+
+#require(MuMIn) #R Squared
+# read https://cran.r-project.org/web/packages/MuMIn/MuMIn.pdf for interpretation
+
 ### DATA MGMT
 # read data
 redcard <- read.csv(here('Data', 'CrowdstormingDataJuly1st.csv'), stringsAsFactors = FALSE)
@@ -45,8 +47,8 @@ mod1 <- glmer(redCards ~ 1 + (1 | playerShort),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 summary(mod1)
-r.squaredGLMM(mod1)
-tab_model(mod1)
+get_variance(mod1, component = "all")
+
 # random intercept for players & referees
 mod1.1 <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum),
               data = redcard,
@@ -54,64 +56,31 @@ mod1.1 <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 summary(mod1.1)
-r.squaredGLMM(mod1.1)
-tab_model(mod1.1)
+get_variance(mod1.1, component = "all")
 
 # compare models
 anova(mod1, mod1.1)
 
-# random intercept for players, referees, & league country
-#mod1x <- glmer(redCards ~ 1 + (1 | playerShort) + (1 | refNum)
-#                + (1 | leagueCountry),
-#               data = redcard,
-#                family = binomial(link="logit"),
-#                control = glmerControl(optimizer = "bobyqa"),
-#                nAGQ = 0)
-#summary(mod1x)
-# no additional variance explained by league country
-
-# random intercept for players, referees, & league country
-# fixed effects of player position and avg skin rating
-mod2 <- glmer(redCards ~ position + avrate + (1 | playerShort) 
+mod2.1 <- glmer(redCards ~  avrate + (1 | playerShort) 
               + (1 | refNum),
                 data = redcard,
                 family = binomial(link="logit"),
                 control = glmerControl(optimizer = "bobyqa"),
                 nAGQ = 0)
-summary(mod2)
-r.squaredGLMM(mod2)
-tab_model(mod2)
+summary(mod2.1)
+get_variance(mod2.1, component = "all")
+
+# random intercept for players and referees
+# fixed effect of avg skin rating and player position
+# NOTE: Including fixed factors using as.factor() does not change the output.
+mod2.2 <- glmer(redCards ~  avrate + position + (1 | playerShort) 
+                + (1 | refNum),
+                data = redcard,
+                family = binomial(link="logit"),
+                control = glmerControl(optimizer = "bobyqa"),
+                nAGQ = 0)
+summary(mod2.2)
+get_variance(mod2.2, component = "all")
 
 # compare models
-anova(mod2, mod2x)
-anova(mod1, mod1.1, mod2)
-
-mod2x <- glmer(redCards ~ as.factor(position) + avrate 
-               + (1 | playerShort) + (1 | refNum),
-              data = redcard,
-              family = binomial(link="logit"),
-              control = glmerControl(optimizer = "bobyqa"),
-              nAGQ = 0)
-summary(mod2x)
-r.squaredGLMM(mod2x)
-tab_model(mod2x)
-
-# Including fixed factors using as.factor() does not change the output.
-
-anova(mod1, mod1.1, mod2, mod2x)
-
-# random intercept for players & referees
-# random slope for players over referees
-# fixed effects of player position
-mod3 <- glmer(redCards ~ position + avrate +  (1 | playerShort) 
-              + (1 | refNum) + (0 + playerShort | refNum),
-               data = redcard,
-               family = binomial(link="logit"),
-               control = glmerControl(optimizer = "bobyqa"),
-               nAGQ = 0)
-# I do not have the computational power to do this:
-# "cannot allocate vector of size 10.9 Gb"
-#sink()
-summary(mod3)
-r.squaredGLMM(mod3)
-tab_model(mod3)
+anova(mod1, mod1.1, mod2.1, mod2.2)
